4a88a643e8f85f33fa8c4c47692ae6690e5d6ed1
Manuel Kiessling <manuel.kiessling@kaufhof.de>
2015-08-29 22:03:46 +0200

    Working on reg workflow and tests

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,10 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="default")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('AppBundle:default:index.html.twig');
     }
 }
