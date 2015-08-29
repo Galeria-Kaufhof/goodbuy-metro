276aec73c2b55a7623a0df52d5f9c9810769a6e4
Manuel Kiessling <manuel@kiessling.net>
2015-08-29 11:57:28 +0200

    First approach to set up continuous delivery to preprod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'preprod'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
