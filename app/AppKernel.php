22929dff06fac887a96db7caffa29f3b329226bd
Manuel Kiessling <manuel.kiessling@kaufhof.de>
2015-08-30 01:53:08 +0200

    Integrated Knp Snappy, Snappy Bundle, wkhtmltopdf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'preprod'))) {
