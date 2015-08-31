b8658dddb0491e4ed6f6c778ce672aba6326567d
Manuel Kiessling <manuel.kiessling@kaufhof.de>
2015-08-31 09:10:05 +0200

    Persistence and mailing work in reg flow

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,6 +21,40 @@ class DefaultController extends Controller
 
         if ($form->isValid()) {
             $this->addFlash('success', 'Vielen Dank. Sie erhalten nun eine Aktivierungsmail.');
+
+            $customer = $form->getData();
+            $customer->setIsActivated(false);
+            $secret = $this->getParameter('secret');
+            $customer->setActivationCode(sha1($secret . $customer->getEmail()));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($customer);
+            $em->flush();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Ihre Registrierung für die Good Bye Metro Sonderaktion')
+                ->setFrom('goodbye-metro@kaufhof.de')
+                ->setTo($customer->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'Emails/activateRegistration.html.twig',
+                        array('activationCode' => $customer->getActivationCode())
+                    ),
+                    'text/html'
+                )
+                /*
+                 * If you also want to include a plaintext version of the message
+                ->addPart(
+                    $this->renderView(
+                        'Emails/registration.txt.twig',
+                        array('name' => $name)
+                    ),
+                    'text/plain'
+                )
+                */
+            ;
+            $this->get('mailer')->send($message);
+
         }
 
         return $this->render(
