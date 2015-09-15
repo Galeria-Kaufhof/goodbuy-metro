d71f2eb9478d2d3161b06927596b938de2a4084e
Manuel Kiessling <manuel.kiessling@kaufhof.de>
2015-09-15 18:34:26 +0200

    Removed sales division Real

--- src/AppBundle/Form/Type/RegistrationType.php
+++ src/AppBundle/Form/Type/RegistrationType.php
@@ -38,7 +38,6 @@ class RegistrationType extends AbstractType
                     'choices' => [
                         Customer::SALESDIVISION_CASH_CARRY => 'METRO Cash & Carry',
                         Customer::SALESDIVISION_MEDIAMARKT_SATURN => 'Media-Saturn',
-                        Customer::SALESDIVISION_REAL => 'Real',
                         Customer::SALESDIVISION_METRO_GROUP_LOGISTIK => 'METRO Group Logistik'
                     ],
                     'required' => true,
@@ -60,7 +59,7 @@ Zeitraum vom 24.-30.09.2015 zum Zwecke der Abwicklung gespeichert und nach Absch
                 'optInAccepted',
                 'checkbox',
                 [
-                    'label' => 'Ja, ich möchte auch weiterhin über spannende Aktionen und Gewinnspiele informiert werden und bin mit der Verwendung meiner erhobenen E-Mail-Adresse zu Marketingzwecken und für die Zusendung von Werbung per E-Mail der folgenden Firmen: GALERIA Kaufhof GmbH, real,- SB Warenhaus GmbH, Metro Cash & Carry Deutschland GmbH, Media Markt E-Business GmbH und der Hudson‘s Bay Company einverstanden. Das Einverständnis kann ich jederzeit widerrufen.',
+                    'label' => 'Ja, ich möchte auch weiterhin über spannende Aktionen und Gewinnspiele informiert werden und bin mit der Verwendung meiner erhobenen E-Mail-Adresse zu Marketingzwecken und für die Zusendung von Werbung per E-Mail der folgenden Firmen: GALERIA Kaufhof GmbH, Metro Cash & Carry Deutschland GmbH, Media Markt E-Business GmbH und der Hudson‘s Bay Company einverstanden. Das Einverständnis kann ich jederzeit widerrufen.',
                     'required' => false,
                     'label_attr' => ['class' => 'non-bold display-inline']
                 ]
